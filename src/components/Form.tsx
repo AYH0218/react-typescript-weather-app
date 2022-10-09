921e519917b522c3a5e8303bdd237957b00617be
AYH0218 <ayuhak0218@gmail.com>
2022-10-09 12:41:39 +0900

    英字判定

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,38 @@
+import { useState } from 'react';
+
 type FormPropsType = {
   setCity: React.Dispatch<React.SetStateAction<string>>;
   getWeather: (e: React.FormEvent<HTMLFormElement>) => void;
 };
 
 const Form = (props: FormPropsType) => {
+  const [nameError, setNameError] = useState<string>('');
+  const handleBlur = (e: any) => {
+    const name = e.target.value;
+    if (!name) {
+      setNameError('Please enter City Name');
+    } else if (name.match(/[^A-Za-z0-9]+/)) {
+      setNameError('英字で入力してください');
+    } else {
+      setNameError('');
+    }
+  };
+  const handleFocus = (e: any) => {
+    setNameError('');
+  };
+
   return (
     <form onSubmit={props.getWeather}>
-      <input type="text" name="city" placeholder="City Name" onChange={(e) => props.setCity(e.target.value)} />
+      <input
+        type="text"
+        name="city"
+        placeholder="City Name"
+        onChange={(e) => props.setCity(e.target.value)}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+      />
       <button type="submit">Get Weather</button>
+      {nameError && <p>{nameError}</p>}
     </form>
   );
 };
